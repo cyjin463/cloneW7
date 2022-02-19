b44dcc672df2c8f8f61a82e8cdac0314ffedc995
devmagrfs <devmagrfs@gmail.com>
2022-02-19 22:07:04 +0900

    redux-persist 모듈 설치 및 수정

--- w7/src/index.js
+++ w7/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './shared/index.css';
 import App from './shared/App';
-
 import { Provider } from "react-redux";
 import store from "./redux/configureStore";
 import { persistStore } from "redux-persist";
